af0c23b62ff1d2bcc7ca7d3442978791795a7996
CrisMen2610 <crisdamencast@gmail.com>
2025-08-13 20:48:50 -0500

    test - commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,4 +11,12 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./features/strategy/strategy.component').then((m) => m.Strategy),
   },
+
+  {
+    path: 'add-account',
+    loadComponent: () =>
+      import('./features/reportForm/report-form.component').then(
+        (m) => m.ReportFormComponent
+      ),
+  },
 ];
