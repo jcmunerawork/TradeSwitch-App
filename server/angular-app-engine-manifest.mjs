2c87a4047146d591487c39411779d80ce2e7b000
JuanEstebanDuque <JuanEstebanDuque@users.noreply.github.com>
2025-10-08 13:36:40 +0000

    deploy: c8c67d3124c11a3b06c29f4849d676f3e000b2cd

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/tradeManager-web',
+  basePath: '/',
   supportedLocales: {
   "en-US": ""
 },
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,7 +2,7 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/tradeManager-web/',
+  baseHref: '/',
   locale: undefined,
   routes: [
   {
@@ -19,7 +19,7 @@ export default {
       "chunk-HJWNCUMO.js",
       "chunk-5E6NCZTH.js"
     ],
-    "route": "/tradeManager-web"
+    "route": "/"
   },
   {
     "renderMode": 2,
@@ -35,7 +35,7 @@ export default {
       "chunk-HJWNCUMO.js",
       "chunk-5E6NCZTH.js"
     ],
-    "route": "/tradeManager-web/strategy"
+    "route": "/strategy"
   },
   {
     "renderMode": 2,
@@ -48,7 +48,7 @@ export default {
       "chunk-SAK2ZTT3.js",
       "chunk-TB2EFIJE.js"
     ],
-    "route": "/tradeManager-web/edit-strategy"
+    "route": "/edit-strategy"
   },
   {
     "renderMode": 2,
@@ -64,7 +64,7 @@ export default {
       "chunk-SAK2ZTT3.js",
       "chunk-EM2IW7TQ.js"
     ],
-    "route": "/tradeManager-web/report"
+    "route": "/report"
   },
   {
     "renderMode": 2,
@@ -76,7 +76,7 @@ export default {
       "chunk-5E6NCZTH.js",
       "chunk-TB2EFIJE.js"
     ],
-    "route": "/tradeManager-web/signup"
+    "route": "/signup"
   },
   {
     "renderMode": 2,
@@ -88,7 +88,7 @@ export default {
       "chunk-5E6NCZTH.js",
       "chunk-TB2EFIJE.js"
     ],
-    "route": "/tradeManager-web/admin-signup"
+    "route": "/admin-signup"
   },
   {
     "renderMode": 2,
@@ -100,7 +100,7 @@ export default {
       "chunk-5E6NCZTH.js",
       "chunk-TB2EFIJE.js"
     ],
-    "route": "/tradeManager-web/login"
+    "route": "/login"
   },
   {
     "renderMode": 2,
@@ -111,7 +111,7 @@ export default {
       "chunk-6B66MAHI.js",
       "chunk-TB2EFIJE.js"
     ],
-    "route": "/tradeManager-web/overview"
+    "route": "/overview"
   },
   {
     "renderMode": 2,
@@ -120,7 +120,7 @@ export default {
       "chunk-K32DDES3.js",
       "chunk-TB2EFIJE.js"
     ],
-    "route": "/tradeManager-web/users"
+    "route": "/users"
   },
   {
     "renderMode": 2,
@@ -132,7 +132,7 @@ export default {
       "chunk-6B66MAHI.js",
       "chunk-TB2EFIJE.js"
     ],
-    "route": "/tradeManager-web/revenue"
+    "route": "/revenue"
   },
   {
     "renderMode": 2,
@@ -145,7 +145,7 @@ export default {
       "chunk-5E6NCZTH.js",
       "chunk-TB2EFIJE.js"
     ],
-    "route": "/tradeManager-web/account"
+    "route": "/account"
   },
   {
     "renderMode": 2,
@@ -158,7 +158,7 @@ export default {
       "chunk-EM2IW7TQ.js",
       "chunk-TB2EFIJE.js"
     ],
-    "route": "/tradeManager-web/trading-accounts"
+    "route": "/trading-accounts"
   },
   {
     "renderMode": 2,
@@ -170,26 +170,26 @@ export default {
       "chunk-5E6NCZTH.js",
       "chunk-TB2EFIJE.js"
     ],
-    "route": "/tradeManager-web/add-account"
+    "route": "/add-account"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1912, hash: '2092986453130c43f31441032a9055660383dc9f2fa2b1e940a8e2da95fb6079', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 2260, hash: '003fed144d6116698fd04632d815284139913c1d94c75eafc2396dd2bde702b9', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 27533, hash: '3d4f06f9d1e79ec4491e885bdb8f39aeb029f9e469aa700050e4fd87be25054a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'strategy/index.html': {size: 27533, hash: '3d4f06f9d1e79ec4491e885bdb8f39aeb029f9e469aa700050e4fd87be25054a', text: () => import('./assets-chunks/strategy_index_html.mjs').then(m => m.default)},
-    'revenue/index.html': {size: 27325, hash: '241d2e6172792d81c097f047dc91d2b7446ec4f55c32cd9baf9215b3e6f20d61', text: () => import('./assets-chunks/revenue_index_html.mjs').then(m => m.default)},
-    'overview/index.html': {size: 27273, hash: '4e1831e6e0588d9b61943d57f73576a6d78b1647176d745ba1e3b798d155e2f3', text: () => import('./assets-chunks/overview_index_html.mjs').then(m => m.default)},
-    'edit-strategy/index.html': {size: 27377, hash: '011da764798ca43ab2083e0e5bd3352896e1f90c6d328d9f18d9d2112621e91f', text: () => import('./assets-chunks/edit-strategy_index_html.mjs').then(m => m.default)},
-    'trading-accounts/index.html': {size: 27377, hash: '0b656b30ae450e9a598cb97b7acd0539b4af72cd936e8328e669bbb4db1d3c64', text: () => import('./assets-chunks/trading-accounts_index_html.mjs').then(m => m.default)},
-    'users/index.html': {size: 27169, hash: 'c14c4c8c47df308a0d061ab9c3a4e00ca9e37f02bf6308513d45288cdf49a675', text: () => import('./assets-chunks/users_index_html.mjs').then(m => m.default)},
-    'add-account/index.html': {size: 27325, hash: '34301188bf81b32a8cd4d53218aee5797c157ca97cfaf07a8ef88d3822a02744', text: () => import('./assets-chunks/add-account_index_html.mjs').then(m => m.default)},
-    'login/index.html': {size: 27325, hash: '1423d8fce11796d407b9c14f0a6cf2bd0cb46c73e7bd3e5dabacc0fb500e9cb1', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
-    'report/index.html': {size: 27533, hash: 'b0cfd8f22dd45ab920f11591c570c65a54eeb644e9f5f11031f2f82b175ecae3', text: () => import('./assets-chunks/report_index_html.mjs').then(m => m.default)},
-    'account/index.html': {size: 27377, hash: '8e1baea0c285d18769bd8ba6809a9a43afc6be629d8c1fc0526290d5b8772c8e', text: () => import('./assets-chunks/account_index_html.mjs').then(m => m.default)},
-    'signup/index.html': {size: 165810, hash: '8010358c3e1ee3605342e774e719fd100e12c00e2ecc8e8855882ca79e0771cf', text: () => import('./assets-chunks/signup_index_html.mjs').then(m => m.default)},
-    'admin-signup/index.html': {size: 165607, hash: '7e3e00eaa29f1e835f6bd98f105f33d0fb4cb57566f3c02e7385d65cfb94034a', text: () => import('./assets-chunks/admin-signup_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1895, hash: '53e60af0db5701b69140cbc1830fae2295eba6e6b2946a052e39118cbef5ae17', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 2243, hash: '3086760f2e740ec8e8bd27425d77f456224c615ca7a7b34c776b218f7188bb06', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 27499, hash: 'a9b849a670cdc5664835007166a6e9fdaf5f89896e570c2d95e0f50c00fb06ed', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'edit-strategy/index.html': {size: 27343, hash: '161ee88f7b93a00f84f61197559637beec54c28dd0ba16f8461eb43604b70cac', text: () => import('./assets-chunks/edit-strategy_index_html.mjs').then(m => m.default)},
+    'signup/index.html': {size: 165777, hash: '2162e948b9738f959fd0cd48acdecd554c2e80ac38fee3f6dfa5d77d1a3b2123', text: () => import('./assets-chunks/signup_index_html.mjs').then(m => m.default)},
+    'login/index.html': {size: 27291, hash: 'b59db7943d494402790a7043adc7302f7d4993abfbab60cfde71a2e1c3e36c9e', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
+    'users/index.html': {size: 27135, hash: '2ae1bccdeb54c8e64c96d82c4b785e86ada1641ffc7972a09d77f37bfbd985de', text: () => import('./assets-chunks/users_index_html.mjs').then(m => m.default)},
+    'account/index.html': {size: 27343, hash: '55e974c841a85e6d7c8bdcf3d37d75ef03f472fcdb346888f02638db74fba3d8', text: () => import('./assets-chunks/account_index_html.mjs').then(m => m.default)},
+    'add-account/index.html': {size: 27291, hash: '74637c3feed73d090fe1f7f1dff442b961deba8631896c1196542e4916220590', text: () => import('./assets-chunks/add-account_index_html.mjs').then(m => m.default)},
+    'report/index.html': {size: 27499, hash: '41b0660a379bd04c3c1bb1982a7a1e766257097a4fe94bd4a6783822ad00602e', text: () => import('./assets-chunks/report_index_html.mjs').then(m => m.default)},
+    'overview/index.html': {size: 27239, hash: 'db024851808c50eacd840d64fcb06f21964cdfc18cf1d45792c3c4e2fb7b4b1f', text: () => import('./assets-chunks/overview_index_html.mjs').then(m => m.default)},
+    'trading-accounts/index.html': {size: 27343, hash: '62b32c8fa8b19395f8c8d580444ee790b9e014cbd08f9642b6a4fd0f800d212b', text: () => import('./assets-chunks/trading-accounts_index_html.mjs').then(m => m.default)},
+    'admin-signup/index.html': {size: 165590, hash: '9e5aaab58fa7f71178ba7fea414bbe0ebe7f760d0c16ccab5eaa79291d4417d0', text: () => import('./assets-chunks/admin-signup_index_html.mjs').then(m => m.default)},
+    'strategy/index.html': {size: 27499, hash: 'a9b849a670cdc5664835007166a6e9fdaf5f89896e570c2d95e0f50c00fb06ed', text: () => import('./assets-chunks/strategy_index_html.mjs').then(m => m.default)},
+    'revenue/index.html': {size: 27291, hash: 'da0a86bd2ec2205b170bcae0e03dd86bee9348ca133a80b2604fb7c7fa5ded46', text: () => import('./assets-chunks/revenue_index_html.mjs').then(m => m.default)},
     'styles-DQD57UPU.css': {size: 28762, hash: '3gTCcubsNy0', text: () => import('./assets-chunks/styles-DQD57UPU_css.mjs').then(m => m.default)}
   },
 };
